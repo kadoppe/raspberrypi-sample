f749108ad72e2b77ed9e1532c275fa1323e13595
kadoppe <kadoppe@me.com>
2015-12-23 15:24:01 +0900

    Change pin number

--- lchika.rb
+++ lchika.rb
@@ -1,14 +1,14 @@
 io = open('/sys/class/gpio/export', 'w')
-io.write(25)
+io.write(21)
 io.close
 
-dir = open('/sys/class/gpio/gpio25/direction', 'w')
+dir = open('/sys/class/gpio/gpio21/direction', 'w')
 dir.write('out')
 dir.close
 
 out = 1
 10.times do
-  v = open('/sys/class/gpio/gpio25/value', 'w')
+  v = open('/sys/class/gpio/gpio21/value', 'w')
   v.write(out)
   v.close
   out = out == 1 ? 0 : 1
@@ -16,5 +16,5 @@ out = 1
 end
 
 uexport = open('/sys/class/gpio/unexport', 'w')
-uexport.write(25)
+uexport.write(21)
 uexport.close
